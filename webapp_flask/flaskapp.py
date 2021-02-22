1a110a9ca6a6b2a66a0b476f3715a69d64f71068
james-cole-174 <71066443+james-cole-174@users.noreply.github.com>
2021-02-22 16:20:34 +0000

    added flask files

---
+++ webapp_flask/flaskapp.py
@@ -0,0 +1,33 @@
+from flask import Flask, escape, request, render_template, url_for 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '201def51bb6b28e0290a585c8eb778d6'
+
+posts = [
+    {
+        'author': 'James',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2022'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+# Auto run site in website if run from this module
+if __name__ == '__main__':
+    app.run(debug=True)
