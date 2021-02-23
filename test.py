99f7408e52dcf0e86129c95032aa1b1f0aa71f30
james-cole-174 <71066443+james-cole-174@users.noreply.github.com>
2021-02-23 11:15:10 +0000

    added testing data

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 import mysqlmodule as msm
 
-#msm.deleteRecord("products", "product_name", "Table")
-msm.addRecordToTable("products", {"product_name":"Sofa", "unit_price":"59.99", "product_msrp":"59.99", "product_description":"It is a sofa. Seats at least 3 people. Cushiony foam, comfy.", "product_weight":"75"})
+product_dictionary = {
+    "product_name":"Sony 55 Inch KD55A8BU Smart 4K UHD HDR OLED Android TV",
+    "unit_price":"1299.00",
+    "product_msrp":"1299.00",
+    "product_description":"The A8 OLED uses Sony best ever picture processor to create stunningly realistic images, and working with Pixel Contrast Booster, colour and contrast is enhanced for extra depth and texture. Sound and picture are in perfect harmony with Acoustic Surface Audio. BRAVIA OLED for incredible contrast, true-to-life colour, a blur-free image and an exceptionally wide viewing angle. The Sony BRAVIA KD55A8 includes the same flagship processor that drives our 8K TVs, analysing the individual objects in each picture and boosting the colour, contrast and clarity.",
+    "product_weight":"18.6"
+}
+
+msm.deleteRecord("products", "product_name", "Table")
+msm.deleteRecord("products", "product_name", "Chair")
+msm.deleteRecord("products", "product_name", "Sofa")
+msm.deleteRecord("products", "product_name", "Bed")
+msm.deleteRecord("products", "product_name", "TV")
+#msm.addRecordToTable("products", product_dictionary)
